4987beb734c455f3758326b02b275200c5335ad7
annamelnik489 <78474754+annamelnik489@users.noreply.github.com>
2022-11-22 04:29:46 +0100

    aggregate by year

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from spyre import server
 from query import DATA_DIR_FORMAT, UKR_IDX
 from pathlib import Path
+import matplotlib.ticker as ticker
+import pandas as pd
+import numpy as np
 
-# from get_data import download_region
+def replace_year(x):
+    print(type(x))
+    return x
 
 
 class VHIApp(server.App):
@@ -23,81 +28,63 @@ class VHIApp(server.App):
         {
             "type": 'dropdown',
             "label": 'Region',
-            "options": [
-                {"label": "Cherkasy", "value": "1"},
-                {"label": "Chernihiv", "value": "2"},
-                {"label": "Chernivtsi", "value": "3"},
-                {"label": "Crimea", "value": "4"},
-                {"label": "Dnipropetrovs'k", "value": "5"},
-                {"label": "Donets'k", "value": "6"},
-                {"label": "Ivano-Frankivs'k", "value": "7"},
-                {"label": "Kharkiv", "value": "8"},
-                {"label": "Kherson", "value": "9"},
-                {"label": "Khmel'nyts'kyy", "value": "10"},
-                {"label": "Kiev", "value": "11"},
-                {"label": "Kirovohrad", "value": "13"},
-                {"label": "Luhans'k", "value": "14"},
-                {"label": "L'viv", "value": "15"},
-                {"label": "Mykolayiv", "value": "16"},
-                {"label": "Odessa", "value": "17"},
-                {"label": "Poltava", "value": "18"},
-                {"label": "Rivne", "value": "19"},
-                {"label": "Sumy", "value": "21"},
-                {"label": "Ternopil'", "value": "22"},
-                {"label": "Transcarpathia", "value": "23"},
-                {"label": "Vinnytsya", "value": "24"},
-                {"label": "Volyn", "value": "25"},
-                {"label": "Zaporizhzhya", "value": "26"},
-                {"label": "Zhytomyr", "value": "27"}],
+            "options": None,
             "value": '1',
             "key": 'region',
             "action_id": "update_data"
         }]
-    controls = [{"type": "hidden",
-                "id": "update_data"}]
+
+    controls = [
+        {
+            "type": "hidden",
+            "id": "update_data"
+        }]
 
     tabs = ["Plot", "Table"]
 
-    # outputs = [
-    #     {
-    #         "type": "table",
-    #         "id": "table_id",
-    #         "control_id": "update_data",
-    #         "tab": "Table",
-    #         "on_page_load": True
-    #     }
-    # ]
+    outputs = [
+        {
+            "type": "plot",
+            "id": "plot",
+            "control_id": "update_data",
+            "tab": "Plot"
+        },
+        {
+            "type": "table",
+            "id": "table_id",
+            "control_id": "update_data",
+            "tab": "Table",
+            "on_page_load": True
+        }
+    ]
 
-    # def getData(self, params):
-    #     ticker = params['ticker']
-    #     if ticker == 'empty':
-    #         ticker = params['custom_ticker'].upper()
+    def getData(self, params):
+        print('getData params = {}'.format(params))
+        idx = params['region']
+        data_glob = Path(data_dir).glob('vhi_id_{}_*.csv'.format(idx))
+        for data_file in data_glob:
+            df = pd.read_csv(data_file, index_col=False, header=1)
+        df = df[['year', 'week', params['vh']]]
+        return df
 
-    #     xchng = "NASD"
-    #     param = {
-    #         'q': ticker,  # Stock symbol (ex: "AAPL")
-    #         'i': "86400",  # Interval size in seconds ("86400" = 1 day intervals)
-    #         'x': xchng,  # Stock exchange symbol on which stock is traded (ex: "NASD")
-    #         'p': "3M"  # Period (Ex: "1Y" = 1 year)
-    #     }
-    #     df =  download_region(param)
-    #     return df
-
-    # def getPlot(self, params):
-    #     df = self.getData(params).drop(['Volume'], axis=1)
-    #     plt_obj = df.plot()
-    #     plt_obj.set_ylabel("Price")
-    #     plt_obj.set_xlabel("Date")
-    #     plt_obj.set_title(params['ticker'])
-    #     return plt_obj.get_figure()
+    def getPlot(self, params):
+        df = self.getData(params).drop(['week'], axis=1)
+        df = df.groupby('year', as_index=False)[params['vh']].mean()
+        plt_obj = df.plot(x='year', y=params['vh'])
+        plt_obj.set_ylabel(params['vh'])
+        plt_obj.set_xlabel("Year")
+        plt_obj.set_title('Average {}'.format(params['vh']))
+        return plt_obj.get_figure()
 
 
-def run_server(data_dir:str):
+def run_server(data_dir: str):
     options = list()
-    for name, idx in UKR_IDX:
-        options.append({"label":name, "value":str(idx)})
-    app = VHIApp(options)
+    for name, idx in UKR_IDX.items():
+        options.append({"label": name, "value": str(idx)})
+    app = VHIApp()
+    app.inputs[1]["options"] = options
     app.launch()
+
 
 if __name__ == '__main__':
     data_dir = Path(DATA_DIR_FORMAT.format(Path.cwd()))
@@ -105,3 +92,4 @@ if __name__ == '__main__':
         run_server(data_dir)
     else:
         print('Data dir {} does not exists'.format(data_dir))
+
